222e890ef56e5026f747de6be7d1cf8b0bcf6bc4
Jonas Merke [JJM] <jonas@jjm.one>
2024-11-05 13:39:51 +0100

    🐛 Fix install script

--- src/install-xmrig.sh
+++ src/install-xmrig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt update
-#apt install wget git build-essential cmake automake libtool autoconf -y
-#git clone https://github.com/xmrig/xmrig.git
-#mkdir xmrig/build && cd xmrig/scripts
-#./build_deps.sh && cd ../build
-#cmake .. -DXMRIG_DEPS=scripts/deps
-#make -j$(nproc)
+apt install wget git build-essential cmake automake libtool autoconf -y
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/scripts
+./build_deps.sh && cd ../build
+cmake .. -DXMRIG_DEPS=scripts/deps
+make -j$(nproc)
